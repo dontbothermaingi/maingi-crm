9bbfad1014c8912349c06588f3e497421c609b64
Denis <denismaingi49@gmail.com>
2025-09-24 22:57:28 +0400

    Set up shadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="App">
+    <div className="p-4">
       <p className="font-bold tetx-4xl">The beginning</p>
+      <Button className="">Click me</Button>
     </div>
   );
 }
