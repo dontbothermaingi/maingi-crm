e6080dccad78d337439a186a84c38d38aa71048f
Denis <denismaingi49@gmail.com>
2025-09-26 11:32:18 +0400

    Added sidebarmenu items

--- packages/client/src/components/app-sidebar.tsx
+++ packages/client/src/components/app-sidebar.tsx
@@ -1,12 +1,23 @@
 import {
+   Bolt,
    Calendar,
    ChevronDown,
    ChevronUp,
+   Disc,
+   Fuel,
    Home,
    Inbox,
+   LayoutDashboard,
+   PiggyBank,
+   Receipt,
    Search,
    Settings,
+   ShoppingCart,
+   Store,
+   Truck,
    User2,
+   Wallet,
+   Wrench,
 } from 'lucide-react';
 
 import {
@@ -16,9 +27,12 @@ import {
    SidebarGroup,
    SidebarGroupContent,
    SidebarGroupLabel,
+   SidebarHeader,
    SidebarMenu,
    SidebarMenuButton,
    SidebarMenuItem,
+   SidebarMenuSub,
+   SidebarMenuSubItem,
 } from '@/components/ui/sidebar';
 import {
    Collapsible,
@@ -64,34 +78,185 @@ const items = [
 export function AppSidebar() {
    return (
       <Sidebar variant="floating">
+         {/* Sidebar header */}
+         <SidebarHeader className="text-2xl border-b">
+            Maingi Books
+         </SidebarHeader>
+
+         {/* Sidebar content */}
          <SidebarContent>
-            <Collapsible defaultOpen className="group/collapsible">
-               <SidebarGroup>
-                  <SidebarGroupLabel className="text-md">
-                     <CollapsibleTrigger className="flex justify-between w-full">
-                        Maingi Books
-                        <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+            <SidebarMenu>
+               <SidebarMenuItem>
+                  <SidebarMenuButton>
+                     <LayoutDashboard /> Dashboard
+                  </SidebarMenuButton>
+               </SidebarMenuItem>
+
+               {/* Vehicles */}
+               <Collapsible>
+                  <SidebarMenuItem>
+                     <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                           <Truck /> Vehicles
+                           <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </SidebarMenuButton>
                      </CollapsibleTrigger>
-                  </SidebarGroupLabel>
-                  <CollapsibleContent>
-                     <SidebarGroupContent>
-                        <SidebarMenu>
-                           {items.map((item) => (
-                              <SidebarMenuItem key={item.title}>
-                                 <SidebarMenuButton asChild>
-                                    <a href={item.url}>
-                                       <item.icon />
-                                       <span>{item.title}</span>
-                                    </a>
-                                 </SidebarMenuButton>
-                              </SidebarMenuItem>
-                           ))}
-                        </SidebarMenu>
-                     </SidebarGroupContent>
-                  </CollapsibleContent>
-               </SidebarGroup>
-            </Collapsible>
+                     <CollapsibleContent>
+                        <SidebarMenuSub>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 View Vehicles
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 Vehicle Fuel Report
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                     </CollapsibleContent>
+                  </SidebarMenuItem>
+               </Collapsible>
+
+               {/* Sales */}
+               <Collapsible>
+                  <SidebarMenuItem>
+                     <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                           <Receipt /> Sales
+                           <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </SidebarMenuButton>
+                     </CollapsibleTrigger>
+                     <CollapsibleContent>
+                        <SidebarMenuSub>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Customers</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Invoices</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Credit Note</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Quote</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 Delivery Note
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                     </CollapsibleContent>
+                  </SidebarMenuItem>
+               </Collapsible>
+
+               {/* Purchases */}
+               <Collapsible>
+                  <SidebarMenuItem>
+                     <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                           <ShoppingCart /> Purchases
+                           <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </SidebarMenuButton>
+                     </CollapsibleTrigger>
+                     <CollapsibleContent>
+                        <SidebarMenuSub>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Vendors</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Bills</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                     </CollapsibleContent>
+                  </SidebarMenuItem>
+               </Collapsible>
+
+               {/* Vehicles */}
+               <Collapsible>
+                  <SidebarMenuItem>
+                     <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                           <Wallet /> Banking
+                           <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </SidebarMenuButton>
+                     </CollapsibleTrigger>
+                     <CollapsibleContent>
+                        <SidebarMenuSub>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 Payments Received
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 Payments Made
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                     </CollapsibleContent>
+                  </SidebarMenuItem>
+               </Collapsible>
+
+               {/* Fuel Manager */}
+               <SidebarMenuItem>
+                  <SidebarMenuButton>
+                     <Fuel /> Fuel Manager
+                  </SidebarMenuButton>
+               </SidebarMenuItem>
+
+               {/* Fuel Manager */}
+               <SidebarMenuItem>
+                  <SidebarMenuButton>
+                     <Bolt /> Spares
+                  </SidebarMenuButton>
+               </SidebarMenuItem>
+
+               {/* Vehicle Repair */}
+               <SidebarMenuItem>
+                  <SidebarMenuButton>
+                     <Wrench /> Vehicle repair
+                  </SidebarMenuButton>
+               </SidebarMenuItem>
+
+               {/* Stores */}
+               <SidebarMenuItem>
+                  <SidebarMenuButton>
+                     <Store /> Stores
+                  </SidebarMenuButton>
+               </SidebarMenuItem>
+
+               {/* Tyres */}
+               <Collapsible>
+                  <SidebarMenuItem>
+                     <CollapsibleTrigger asChild>
+                        <SidebarMenuButton>
+                           <Disc /> Tyres
+                           <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </SidebarMenuButton>
+                     </CollapsibleTrigger>
+                     <CollapsibleContent>
+                        <SidebarMenuSub>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>New Tyres</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>Used Tyres</SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                           <SidebarMenuSubItem>
+                              <SidebarMenuButton>
+                                 Retread Tyres
+                              </SidebarMenuButton>
+                           </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                     </CollapsibleContent>
+                  </SidebarMenuItem>
+               </Collapsible>
+            </SidebarMenu>
          </SidebarContent>
+
+         {/* Sidebar Footer */}
          <SidebarFooter>
             <SidebarMenu className="">
                <SidebarMenuItem>
